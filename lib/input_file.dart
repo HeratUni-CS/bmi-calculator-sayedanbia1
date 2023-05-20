02c84be9c7a846cd77844de38910e8698bdb01ad
sayedanbia1 <sayedanbia5555@gamil.com>
2023-05-20 22:52:44 +0430

    used enum class in the project

--- lib/input_file.dart
+++ lib/input_file.dart
@@ -3,14 +3,22 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'reausable.dart';
 import 'containercontent.dart';
 
+const bottomcontainercolor= Color(0xFFEB1555);
 const activecolor= Color(0xFF0A0D22);
+const inactivecolor=Color(0xFF1D1F33);
+
+enum gender{
+  male,
+  female,
+}
 
 class InputPage extends StatefulWidget {
   @override
   State<InputPage> createState() => _InputPageState();
 }
-
 class _InputPageState extends State<InputPage> {
+ gender? sg ;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,12 +31,25 @@ class _InputPageState extends State<InputPage> {
                 child: Row(
               children: [
                 reusableWidget(
-                  color: activecolor,
-                  child: containercontent(iconData: FontAwesomeIcons.mars,label: "Man"),
+                  onpressed: () {
+                    setState(() {
+                      sg=gender.male;
+                    });
+                  },
+                      color: (sg == gender.male) ? activecolor :inactivecolor,
+                      child: containercontent(iconData: FontAwesomeIcons.mars,
+                      label: "Man"
+                  ),
                 ),
                 reusableWidget(
-                  color: activecolor,
-                  child: containercontent(iconData: FontAwesomeIcons.venus,label: "Female"),
+                  onpressed: (){
+                    setState(() {
+                      sg=gender.female;
+                    });
+                  },
+                      color: (sg == gender.female)? activecolor :inactivecolor,
+                      child: containercontent(iconData: FontAwesomeIcons.venus,
+                      label: "Female"),
                 ),
               ],
             )),
--- lib/reausable.dart
+++ lib/reausable.dart
@@ -1,17 +1,21 @@
 import 'package:flutter/material.dart';
 
 class reusableWidget extends StatelessWidget {
-  reusableWidget({required this.color, this.child});
+  reusableWidget({required this.color, this.child, this.onpressed});
   final Color color;
   final Widget? child;
+  final void Function()? onpressed;
   @override
   Widget build(BuildContext context) {
     return Expanded(
-        child: Container(
-          margin: EdgeInsets.all(15),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10),
-            color: color,
+        child: InkWell(
+          onTap: onpressed ,
+          child: Container(
+            margin: EdgeInsets.all(15),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              color: color,
+            ),
           ),
         ));
   }
