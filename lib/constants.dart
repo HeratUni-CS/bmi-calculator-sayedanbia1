c67f08f152016d421e6c91a63866404218aa034e
sayedanbia1 <sayedanbia5555@gamil.com>
2023-05-21 15:45:33 +0430

    three container is compelement

--- lib/constants.dart
+++ lib/constants.dart
@@ -6,7 +6,7 @@ const KLabelstyle = TextStyle(
   color: Color(0xFF868892),
 );
 
-
+const KStylenumber=TextStyle(fontSize: 50)
 const KBottomcontainercolor= Color(0xFFEB1555);
 const KActivecolor= Color(0xFF0A0D22);
 const KInactivecolor=Color(0xFF1D1F33);
--- lib/input_file.dart
+++ lib/input_file.dart
@@ -15,6 +15,7 @@ class InputPage extends StatefulWidget {
 }
 class _InputPageState extends State<InputPage> {
  gender? sg ;
+ int height =175;
 
   @override
   Widget build(BuildContext context) {
@@ -51,11 +52,44 @@ class _InputPageState extends State<InputPage> {
               ],
             )),
             reusableWidget(
-
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Text("Height",style: KLabelstyle,),
+                  Text("Height",style: KLabelstyle,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.baseline,
+                    textBaseline: TextBaseline.alphabetic,
+                    children: [
+                      Text(
+                        height.toString(),
+                        style: KStylenumber,
+                      ),
+                      Text("cm",style: KLabelstyle,),
+                    ],
+                  ),
+                  SliderTheme(
+                    data: SliderThemeData(
+                      trackHeight: 1,
+                      thumbShape: RoundSliderThumbShape(enabledThumbRadius: 15),
+                      overlayShape: RoundSliderOverlayShape(overlayRadius: 30),
+                      activeTrackColor: Colors.white,
+                      inactiveTrackColor: Color(0xFF888993),
+                      thumbColor: Color(0xFFEB1555),
+                      overlayColor: Color(0x27EB1555),
+                    ),
+                    child: Slider(
+                        value:height.toDouble(),
+                        onChanged:(double value) {
+                          setState(() {
+                            height=value.round();
+                          });
+                        },
+                        min: 120,
+                        max: 220,
+                    ),
+                  ),
                 ],
               ),
                 color: KActivecolor
